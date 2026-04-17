7d7690f57c60464793ded3810e1767dc795fc607
thiagoassuncaodev <thiagoevil3092@gmail.com>
2026-04-17 11:46:25 -0300

    primeiro commit

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Esta linha faz o servidor "entregar" seu HTML, CSS e JS do Halloween
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ... resto do código do Stripe aqui ...
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.listen(3000, () => {
-    console.log("Servidor assombrado rodando em http://localhost:3000 🎃");
+    console.log("http://localhost:3000 ");
 });
