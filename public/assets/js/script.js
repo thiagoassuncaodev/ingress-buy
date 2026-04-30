1db1a5c10f31e2747c7cca0d4c66297edd40382d
thiagoassuncaodev <thiagoevil3092@gmail.com>
2026-04-29 23:04:24 -0300

    coleta dos dados apenas para o front pensando em coletar em forma de objeto

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,8 +2,15 @@ const modal = document.getElementById('ticketModal')
 const modalfrm = document.getElementById('modalForm')
 const botoes = document.querySelectorAll('.select-ticket') // por eu ter usado query selector all ele me retorna um nodelist que se assemelha a um vetor
 
+
+window.onload = () => {
+  modal.style.display = 'none'
+  modalfrm.style.display = 'none'
+}
+
 function abrirModal(){
     modal.style.display = 'flex'
+    
 }
 
 function fecharModal(){
@@ -24,3 +31,30 @@ for(let i = 0;i < botoes.length;i++){
     abrirFormModal()
   })
   }
+
+
+const frm = document.getElementById('pedido')
+
+frm.addEventListener('submit',(event) =>{
+  event.preventDefault()
+  
+    const formDados = new FormData(frm)
+    const nome = formDados.get('inNome')
+    const email = formDados.get('inEmail')
+    const telefone = formDados.get('inTelefone')
+    const cpf = formDados.get('inCpf')
+    const quantidade = formDados.get('inQuantidade')
+
+    console.log(nome,email,telefone,cpf,quantidade)
+
+})
+
+let tipoSelecionado = null
+
+botao.addEventListener('click',(event) => {
+  const ticket = event.target.closest('.ticket')
+  tipoSelecionado = ticket.dataset.type
+
+  fecharModal()
+  abrirFormModal()
+})
